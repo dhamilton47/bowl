ae603743681be27fcfc2ee9238153b92cfad7217
Dan J. Hamilton <20844705+dhamilton47@users.noreply.github.com>
2021-06-17 03:19:21 -0400

    Enhancement: additional functionality from Theme UI

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import { hot } from 'react-hot-loader';
+import { ThemeProvider } from 'theme-ui';
+import theme from '../theme'
+
 const AppWrapped = hot(module)(
   () => {
     return (
+      <ThemeProvider theme={theme}>
         <BrowserRouter>
           <App />
         </BrowserRouter>        
+      </ThemeProvider>
     )
   }
 )
