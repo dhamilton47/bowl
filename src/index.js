e200fb314ce7bd416ab928230995f5def8e6e488
Dan J. Hamilton <20844705+dhamilton47@users.noreply.github.com>
2021-06-17 03:15:44 -0400

    Update index.js
    Add react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
+import { hot } from 'react-hot-loader';
+const AppWrapped = hot(module)(
+  () => {
+    return (
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>        
+    )
+  }
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppWrapped />
   </React.StrictMode>,
+  
   document.getElementById('root')
 )
