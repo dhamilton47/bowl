48f4fa76e1878f53574f5eb18ed4b112eab86b61
Dan J. Hamilton <20844705+dhamilton47@users.noreply.github.com>
2021-06-23 14:06:52 -0400

    Update ball.model.js
    Simplify definition as per Sequelize docs.

--- src/models/ball.model.js
+++ src/models/ball.model.js
@@ -1,8 +1,17 @@
-module.exports = (sequelize, Sequelize) => {
+module.exports = (sequelize, Sequelize, DataTypes) => {
   const Ball = sequelize.define("ball", {
-    pins: {
-      type: Sequelize.INTEGER
-    }
+    frameId: DataTypes.INTEGER,
+    pins: DataTypes.INTEGER,
+    pin1: DataTypes.BOOLEAN,
+    pin2: DataTypes.BOOLEAN,
+    pin3: DataTypes.BOOLEAN,
+    pin4: DataTypes.BOOLEAN,
+    pin5: DataTypes.BOOLEAN,
+    pin6: DataTypes.BOOLEAN,
+    pin7: DataTypes.BOOLEAN,
+    pin8: DataTypes.BOOLEAN,
+    pin9: DataTypes.BOOLEAN,
+    pin10: DataTypes.BOOLEAN,
   });
 
   return Ball;
