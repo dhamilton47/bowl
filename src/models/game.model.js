70465c0639f8f95bcf913bb24b4221ef21f80432
Dan J. Hamilton <20844705+dhamilton47@users.noreply.github.com>
2021-06-23 14:06:30 -0400

    Update game.model.js
    Simplify definition as per Sequelize docs.

--- src/models/game.model.js
+++ src/models/game.model.js
@@ -1,50 +1,19 @@
-module.exports = (sequelize, Sequelize) => {
+module.exports = (sequelize, Sequelize, DataTypes) => {
   const Game = sequelize.define("game", {
-    player_name: {
-      type: Sequelize.STRING
-    },
-    player_id: {
-      type: Sequelize.INTEGER
-    },
-    team_id: {
-      type: Sequelize.INTEGER
-    },
-    match_id: {
-      type: Sequelize.INTEGER
-    },
-    frame1: {
-      type: Sequelize.INTEGER
-    },
-    frame2: {
-      type: Sequelize.INTEGER
-    },
-    frame3: {
-      type: Sequelize.INTEGER
-    },
-    frame4: {
-      type: Sequelize.INTEGER
-    },
-    frame5: {
-      type: Sequelize.INTEGER
-    },
-    frame6: {
-      type: Sequelize.INTEGER
-    },
-    frame7: {
-      type: Sequelize.INTEGER
-    },
-    frame8: {
-      type: Sequelize.INTEGER
-    },
-    frame9: {
-      type: Sequelize.INTEGER
-    },
-    frame10: {
-      type: Sequelize.INTEGER
-    },
-    total_score: {
-      type: Sequelize.INTEGER
-    }
+    matchId: DataTypes.INTEGER,
+    playerId: DataTypes.INTEGER,
+    frame1: DataTypes.INTEGER,
+    frame2: DataTypes.INTEGER,
+    frame3: DataTypes.INTEGER,
+    frame4: DataTypes.INTEGER,
+    frame5: DataTypes.INTEGER,
+    frame6: DataTypes.INTEGER,
+    frame7: DataTypes.INTEGER,
+    frame8: DataTypes.INTEGER,
+    frame9: DataTypes.INTEGER,
+    frame10: DataTypes.INTEGER,
+    total_score: DataTypes.INTEGER,
+    is_final: DataTypes.BOOLEAN
   });
 
   return Game;
