77be75c647c6576b25b043c767050520475a6ce5
Dan J. Hamilton <20844705+dhamilton47@users.noreply.github.com>
2021-06-23 14:23:27 -0400

    Refactor: make consistent where possible to other db index components

--- src/components/Team/TeamsList.jsx
+++ src/components/Team/TeamsList.jsx
@@ -1,36 +1,37 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { useState, useEffect } from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import AddTeam from './AddTeam';
-import TeamDataService from '../../services/team.service';
+import DataService from '../../services/team.service';
 
 export default () => {
-  const [searchTeam, setSearchTeam] = useState('');
-  const [teamList, setTeamList] = useState([]);
-  const [highlightedTeam, setHighlightedTeam] = useState(null);
-  const [highlightedTeamIndex, setHighlightedTeamIndex] = useState(-1)
+  const [search, setSearch] = useState('');
+  const [list, setList] = useState([]);
+  const [current, setCurrent] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1)
   
-  useEffect(() => {
-    TeamDataService.getAll()
-        .then(res => {
-          setTeamList(res.data);
-          console.log("res.data:",res.data);
-        })
-        .catch(e => {
-          console.log(e);
-        })
-    console.log("teams:",teamList)
-  },[]);
+  useEffect(() => retreiveList(), []);
   
-  const refreshPointer = () => {
-    setHighlightedTeam(null);
-    setHighlightedTeamIndex(-1);
+  const retreiveList = () => {
+    DataService.getAll()
+      .then(res => {
+        setList(res.data);
+        console.log("res.data:",res.data);
+      })
+      .catch(e => {
+        console.log(e);
+      })
   }
 
-  const onClickDeleteAll = () => {
-    TeamDataService.deleteAll()
+  const refreshPointer = () => {
+    retreiveList();
+    setCurrent(null);
+    setCurrentIndex(-1);
+  }
+
+  const onDeleteAll = () => {
+    DataService.deleteAll()
       .then(res => {
         console.log(res.data);
         refreshPointer();
@@ -38,22 +39,22 @@ export default () => {
       .catch(e => console.log(e));
   }
 
-  const onChangeSearchInput = (e) => {
-    const searchInput = e.target.value;
-    setSearchTeam(searchInput);
+  const onChangeSearch = (e) => {
+    const search = e.target.value;
+    setSearch(search);
   }
 
   function searchTeam1() {
-    setTeams(TeamDataService.findBySchoolName(searchTeam));
+    setTeams(DataService.findBySchoolName(searchTeam));
     console.log(teams);
   }
 
   const editTeamInfo = () => {}
 
-  const setActiveTeam = (team, index) => {
-    setHighlightedTeam(team);
-    setHighlightedTeamIndex(index);
-    console.log(highlightedTeam)
+  const setActive = (value, index) => {
+    setCurrent(value);
+    setCurrentIndex(index);
+    console.log(value)
   }
 
   return (
@@ -64,8 +65,8 @@ export default () => {
         <input
           type="text"
           placeholder="Search by high school"
-          value={searchTeam}
-          onChange={onChangeSearchInput}
+          value={search}
+          onChange={onChangeSearch}
           sx={{width:400, height: 30}} 
         />
         <span>
@@ -80,7 +81,6 @@ export default () => {
 
       <div sx={{
         alignItems: `flex-start`,
-        border: `scaffold`,
         display: `flex`,
         flexDirection: `row`,
         justifyContent: `flex-start`,
@@ -88,7 +88,7 @@ export default () => {
       }}>
 
         {/* Team List */}
-        <div sx={{border: `scaffold`, width: `50%`, minHeight: `20rem`, maxHeight: `32rem`}}>
+        <div sx={{width: `50%`, minHeight: `20rem`, maxHeight: `32rem`, position: `relative`}}>
           <h3>High School Teams</h3>
 
           <div>
@@ -102,9 +102,9 @@ export default () => {
                 width: `90%`,
               }}
             >
-              {teamList && teamList.map(team => (
+              {list && list.map((team, index) => (
                 <li
-                  key={team.id}
+                  key={index}
                   sx={{
                     borderBottom: `lightgray 1px solid`,
                     cursor: `pointer`,
@@ -116,7 +116,7 @@ export default () => {
                       color: `white`
                     },
                   }}
-                  onClick={() => setActiveTeam(team.school_name, team.id)}
+                  onClick={() => setActive(team, index)}
                 >
                   {team.school_name}
                 </li>
@@ -124,7 +124,7 @@ export default () => {
             </ul>
           </div>
 
-          <nav>
+          <nav sx={{ position: `absolute`, bottom: 0}}>
             <div
               sx={{
                 display: `inline-block`,
@@ -133,7 +133,7 @@ export default () => {
             >
               <li
                 sx={{
-                  variant: 'lis.success_medium',
+                  variant: `lis.success_medium`,
                   display: `inline-block`,
                 }}
               >
@@ -151,10 +151,10 @@ export default () => {
 
               <li
                 sx={{
-                  variant: 'lis.warning_medium',
+                  variant: `lis.warning_medium`,
                   display: `inline-block`,
                 }}
-                onClick={onClickDeleteAll}
+                onClick={onDeleteAll}
               >
                 Delete All Teams
               </li>
@@ -164,42 +164,64 @@ export default () => {
         </div>
 
         {/* Team Detail */}
-        <div
-          sx={{
-            border: `scaffold`,
-            minHeight: `20rem`,
-            width: `49%`
-          }}
-        >
-          <div>
-            <div sx={{ display: `block` }}>
-              <h3>Selected Team Details</h3>
-              <div sx={{ display: `block` }}><strong>School Name:</strong>{}</div>
-              <div sx={{ display: `block` }}><strong>Mascot:</strong>{}</div>
-              <div sx={{ display: `block` }}><strong>City:</strong>{}</div>
-              <div sx={{ display: `block` }}><strong>State:</strong>{}</div>
-            </div>
-          </div>
-
-          <nav>
+        {
+          current ?
             <div
               sx={{
-                display: `inline-block`,
-                width: `100%`
+                minHeight: `20rem`,
+                position: `relative`,
+                width: `49%`
               }}
             >
-              <li
-                sx={{
-                  variant: 'lis.caution_small',
-                  display: `inline-block`,
-                }}
-                onClick={editTeamInfo}
-              >
-                Edit
-              </li>
-            </div>        
-          </nav>
-        </div>
+              <div>
+                <div sx={{ display: `block` }}>
+                  <h3>Selected Team Details</h3>
+                  <div sx={{ display: `block` }}><strong>School Name:</strong>{current.school_name}</div>
+                  <div sx={{ display: `block` }}><strong>Mascot:</strong>{current.school_mascot}</div>
+                  <div sx={{ display: `block` }}><strong>City:</strong>{current.city}</div>
+                  <div sx={{ display: `block` }}><strong>State:</strong>{current.state}</div>
+                </div>
+              </div>
+
+              <nav sx={{ position: `absolute`, bottom: 0}}>
+                <div
+                  sx={{
+                    display: `inline-block`,
+                    width: `100%`
+                  }}
+                >
+                  <li
+                    sx={{
+                      variant: `lis.primary_small`,
+                      display: `inline-block`,
+                    }}
+                  >
+                    <Link
+                      to={'/team-roster'}
+                      sx={{
+                        color: `inherit`,
+                        fontSize: `14px`,
+                        textDecoration: `none`,
+                      }}
+                    >
+                      Roster
+                    </Link>
+                  </li>
+
+                  <li
+                    sx={{
+                      variant: `lis.caution_small`,
+                      display: `inline-block`,
+                    }}
+                    onClick={editTeamInfo}
+                  >
+                    Edit
+                  </li>
+                </div>        
+              </nav>
+            </div>
+          : null
+        }
       </div>
     </div>
   )
